c1cc9650c7d0c868472bec4e8b834e4b58a02bca
predevho <dlagusgh7894@gmail.com>
2026-04-08 12:21:01 +0900

    배열의 데이터 수정

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,42 +5,39 @@ import java.util.Arrays;
 class Main {
     public static void main(String[] args) {
         System.out.println("== No Stream ==");
-
         noStreamVersion();
 
         System.out.println("== Stream ==");
-
         streamVersion();
     }
 
     private static void noStreamVersion() {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        String[] messages = new String[]{"1번", "2번", "3번"};
+        int[] numbers = new int[messages.length];
 
-        System.out.println("= 원본 numbers(변형 전) =");
-        System.out.println(Arrays.toString(numbers));
+        for (int i = 0; i < messages.length; i++) {
+            String message = messages[i];
+            // message 에서 가장 마지막 한자를 제거
+            message = message.substring(0, message.length() - 1);
+            int number = Integer.parseInt(message);
 
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] *= 2;
+            if (number % 2 == 0) continue;
+
+            numbers[i] = number;
         }
 
-        System.out.println("= 원본 numbers(변형 후) =");
+        System.out.println("= 결과 =");
         System.out.println(Arrays.toString(numbers));
     }
 
     private static void streamVersion() {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        System.out.println("= 원본 numbers(스트림 전) =");
-        System.out.println(Arrays.toString(numbers));
-
-        int[] calculatedNumbers = Arrays.stream(numbers)
-                .map(e -> e * 2)
+        int[] array = Arrays.stream(new String[]{"1번", "2번", "3번"})
+                .map(e -> e.substring(0, e.length() - 1))
+                .mapToInt(Integer::parseInt)
+                .filter(number -> number % 2 != 0)
                 .toArray();
 
-        System.out.println("= 원본 numbers(스트림 후) =");
-        System.out.println(Arrays.toString(numbers));
-
-        System.out.println("= 새 배열 calculatedNumbers =");
-        System.out.println(Arrays.toString(calculatedNumbers));
+        System.out.println("= 결과 =");
+        System.out.println(Arrays.toString(array));
     }
 }
