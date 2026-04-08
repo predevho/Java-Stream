84c84c845010321c1e6fde5afce957402dd77d74
predevho <dlagusgh7894@gmail.com>
2026-04-08 12:48:09 +0900

    래퍼런스 타입에서 int 타입으로 매핑

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,43 +1,25 @@
 package com;
 
 import java.util.Arrays;
+import java.util.List;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("== No Stream ==");
-        noStreamVersion();
+        // v1
+        List<Integer> list1 = Arrays
+                .stream(new int[]{10, 20, 30})
+                .mapToObj(e -> e) // int => Integer
+                .toList();
 
-        System.out.println("== Stream ==");
-        streamVersion();
-    }
+        // v2
+        List<Integer> list2 = Arrays
+                .stream(new int[]{10, 20, 30})
+                .boxed() // int => Integer
+                .toList();
 
-    private static void noStreamVersion() {
-        String[] messages = new String[]{"1번", "2번", "3번"};
-        int[] numbers = new int[messages.length];
-
-        for (int i = 0; i < messages.length; i++) {
-            String message = messages[i];
-            // message 에서 가장 마지막 한자를 제거
-            message = message.substring(0, message.length() - 1);
-            int number = Integer.parseInt(message);
-
-            if (number % 2 == 0) continue;
-
-            numbers[i] = number;
-        }
-
-        System.out.println("= 결과 =");
-        System.out.println(Arrays.toString(numbers));
-    }
-
-    private static void streamVersion() {
-        int[] array = Arrays.stream(new String[]{"1번", "2번", "3번"})
-                .map(e -> e.substring(0, e.length() - 1))
-                .mapToInt(Integer::parseInt)
-                .filter(number -> number % 2 != 0)
-                .toArray();
-
-        System.out.println("= 결과 =");
-        System.out.println(Arrays.toString(array));
+        // v3 : 이 경우는 래핑(boxing)할 필요가 없다.
+        List<Integer> list3 = Arrays
+                .stream(new Integer[]{10, 20, 30})
+                .toList();
     }
 }
